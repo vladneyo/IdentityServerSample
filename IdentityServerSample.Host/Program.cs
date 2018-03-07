6067ba27e797dc294967ec7dba713fabb1bc813c
KYIV\vlne <vlne@ciklum.com>
2018-03-07 12:52:00 +0200

    Identity Server host was created

---
+++ IdentityServerSample.Host/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace IdentityServerSample.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "IdentityServer";
+
+            BuildWebHost(args).Run();
+
+            Console.WriteLine("Identity Server started!");
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ IdentityServerSample.Host/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityServerSample.Host
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddIdentityServer();
+            builder.AddInMemoryPersistedGrants();
+            builder.AddInMemoryClients(new List<Client> { });
+            builder.AddInMemoryIdentityResources(new List<IdentityResource> { });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseIdentityServer();
+        }
+    }
+}
